3a3855a6fb8c33c577b55f1988dae4396bbc2a88
AlanRandon <alan.randon@outlook.com>
2023-03-16 18:43:13 +0000

    continue with leptos

--- crates/full-stack-web/src/main.rs
+++ crates/full-stack-web/src/main.rs
@@ -1,20 +1,16 @@
+use full_stack_web::app::{App, AppProps};
+
 #[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
     use axum::{extract::Extension, routing::post, Router};
+    use full_stack_web::fileserv::file_and_error_handler;
     use leptos::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
-    use start_axum::app::*;
-    use start_axum::fileserv::file_and_error_handler;
     use std::sync::Arc;
 
-    simple_logger::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
+    simple_logger::init_with_level(log::Level::Info).expect("couldn't initialize logging");
 
-    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
-    // For deployment these variables are:
-    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
-    // Alternately a file can be specified such as Some("Cargo.toml")
-    // The file would need to be included with the executable when moved to deployment
     let conf = get_configuration(None).await.unwrap();
     let leptos_options = conf.leptos_options;
     let addr = leptos_options.site_addr;
@@ -37,5 +33,4 @@ async fn main() {
 }
 
 #[cfg(not(feature = "ssr"))]
-pub fn main() {
-}
+pub fn main() {}
