2b9002167433fbb7811ba32551c065a44e4c1972
AlanRandon <alan.randon@outlook.com>
2023-03-17 16:57:36 +0000

    continue to try to understand leptos

--- crates/full-stack-web/src/lib.rs
+++ crates/full-stack-web/src/lib.rs
@@ -1,24 +1,19 @@
-use cfg_if::cfg_if;
-use leptos::*;
 pub mod app;
 pub mod error_template;
 
-#[cfg(feature = "ssr")]
-pub mod fileserv;
+// #[cfg(feature = "ssr")]
+// pub mod fileserv;
 
-cfg_if! {
-    if #[cfg(feature = "hydrate")] {
-        use wasm_bindgen::prelude::wasm_bindgen;
-        use crate::app::*;
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    use crate::app::*;
+    use leptos::view;
 
-        #[wasm_bindgen]
-        pub fn hydrate() {
-            _ = console_log::init_with_level(log::Level::Debug);
-            console_error_panic_hook::set_once();
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
 
-            leptos::mount_to_body(move |cx| {
-                view! { cx, <App/> }
-            });
-        }
-    }
+    leptos::mount_to_body(move |cx| {
+        view! { cx, <App/> }
+    });
 }
--- crates/full-stack-web/src/main.rs
+++ crates/full-stack-web/src/main.rs
@@ -1,13 +1,27 @@
 use full_stack_web::app::{App, AppProps};
 
 #[cfg(feature = "ssr")]
+use {
+    axum::{
+        body::Body, extract::Extension, http::Request, response::IntoResponse, routing::post,
+        Router,
+    },
+    full_stack_web::error_template::{AppError, ErrorTemplate, ErrorTemplateProps},
+    leptos::{get_configuration, view},
+    leptos_axum::{generate_route_list, LeptosRoutes},
+    std::sync::Arc,
+    tower::{ServiceBuilder, ServiceExt},
+    tower_http::services::ServeDir,
+};
+
+#[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
-    use axum::{extract::Extension, routing::post, Router};
-    use full_stack_web::fileserv::file_and_error_handler;
-    use leptos::*;
-    use leptos_axum::{generate_route_list, LeptosRoutes};
-    use std::sync::Arc;
+    use axum::{
+        error_handling::HandleErrorLayer,
+        handler::HandlerService,
+        routing::{get_service, MethodRouter},
+    };
 
     simple_logger::init_with_level(log::Level::Info).expect("couldn't initialize logging");
 
@@ -16,16 +30,28 @@ async fn main() {
     let addr = leptos_options.site_addr;
     let routes = generate_route_list(|cx| view! { cx, <App/> }).await;
 
-    // build our application with a route
     let app = Router::new()
+        // handle leptos server fns
         .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
+        // handle leptos routes
         .leptos_routes(leptos_options.clone(), routes, |cx| view! { cx, <App/> })
-        .fallback(file_and_error_handler)
-        .layer(Extension(Arc::new(leptos_options)));
+        .fallback({
+            let root = leptos_options.site_root.clone();
+            let handle_error = leptos_axum::render_app_to_stream(
+                leptos_options.clone(),
+                move |cx| view! {cx, <ErrorTemplate error=AppError::NotFound/>},
+            );
+            get_service(ServeDir::new(root).then(|response| async move {
+                match response {
+                    Ok(response) => reponse,
+                    Err(err) => handle_error().
+                }
+                
+            }))
+        });
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    log!("listening on http://{}", &addr);
+    log::info!("listening on http://{}", &addr);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
