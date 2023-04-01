bd237159ab35e6f7e9804f6a99cfcaab53e90874
AlanRandon <alan.randon@outlook.com>
2023-04-01 16:01:03 +0100

    get game of life to work

--- crates/game-of-life-leptos/src/app.rs
+++ crates/game-of-life-leptos/src/app.rs
@@ -2,11 +2,7 @@ use game_of_life_core::{prelude as game_of_life, state::CellState};
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
-use std::{
-    cell::Cell,
-    sync::{Arc, RwLock, RwLockWriteGuard},
-    time::Duration,
-};
+use std::sync::{Arc, Mutex, RwLock};
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
@@ -28,18 +24,44 @@ pub fn App(cx: Scope) -> impl IntoView {
     }
 }
 
+/// A function to toggle the internal and signal state of one cell
+type CellToggleFunction = Arc<Mutex<Box<dyn FnMut()>>>;
+
 struct StateWrapper {
-    state: game_of_life::State,
-    pub signals: Vec<(ReadSignal<CellState>, WriteSignal<CellState>)>,
+    state: Arc<RwLock<game_of_life::State>>,
+    signals: Vec<(
+        ReadSignal<CellState>,
+        WriteSignal<CellState>,
+        CellToggleFunction,
+    )>,
 }
 
 impl StateWrapper {
     fn new(cx: Scope, state: game_of_life::State) -> Self {
-        let signals = create_many_signals(cx, state.cells());
+        let cells = state.cells();
 
-        for (index, (cell_state, set_cell_state)) in signals.iter().enumerate() {
-            create_effect(cx, move |_| state.replace_at_index(index, cell_state.get()))
-        }
+        let state = Arc::new(RwLock::new(state));
+
+        let signals = create_many_signals(cx, cells)
+            .into_iter()
+            .enumerate()
+            .map(|(index, (cell_state, set_cell_state_internal))| {
+                let state = Arc::clone(&state);
+                let set_cell_state = move || {
+                    let new_cell_state = !cell_state.get();
+                    state
+                        .write()
+                        .unwrap()
+                        .replace_at_index(index, new_cell_state);
+                    set_cell_state_internal.set(new_cell_state);
+                };
+                (
+                    cell_state,
+                    set_cell_state_internal,
+                    Arc::from(Mutex::new(Box::new(set_cell_state) as Box<dyn FnMut()>)),
+                )
+            })
+            .collect();
 
         Self { signals, state }
     }
@@ -51,39 +73,44 @@ impl StateWrapper {
             state: new_state,
             coordinates,
             needs_rerender,
-        } in state.next().unwrap().into_iter()
+        } in state.write().unwrap().next().unwrap().into_iter()
         {
             if !needs_rerender {
                 continue;
             }
 
-            signals[coordinates.to_index(20) as usize]
-                .1
-                .update_untracked(|state| *state = new_state)
+            let (_, set_state_internal, _) = signals[coordinates.to_index(20) as usize];
+
+            set_state_internal.update(|state| *state = new_state);
         }
+    }
+
+    fn cell_signals(&self) -> Vec<(ReadSignal<CellState>, CellToggleFunction)> {
+        self.signals
+            .iter()
+            .map(|(state, _, set_state)| (*state, set_state.clone()))
+            .collect()
     }
 }
 
 #[component]
 fn HomePage(cx: Scope) -> impl IntoView {
     let mut state = StateWrapper::new(cx, game_of_life::State::new(20, 20));
-
-    let (state_signal, set_state_signal) = create_signal(cx, ());
+    let cells = state.cell_signals();
 
     let (should_update, set_should_update) = create_signal(cx, false);
 
-    let change_state = {
-        move || {
-            if !should_update.get() {
-                return;
-            }
+    #[allow(unused_variables)]
+    let tick = move || {
+        if should_update.get() {
+            state.next_state();
         }
     };
 
     #[cfg(target_arch = "wasm32")]
     {
-        let callback = Closure::wrap(Box::new(change_state) as Box<dyn FnMut()>).into_js_value();
-        let interval = Duration::from_millis(100);
+        let callback = Closure::wrap(Box::new(tick) as Box<dyn FnMut()>).into_js_value();
+        let interval = std::time::Duration::from_millis(100);
         window()
             .set_interval_with_callback_and_timeout_and_arguments_0(
                 callback.as_ref().unchecked_ref(),
@@ -97,15 +124,15 @@ fn HomePage(cx: Scope) -> impl IntoView {
         <div class="p-4 grid place-items-center h-full w-full">
             <div class="p-4 grid place-items-center text-white rounded gap-2 [grid-template-columns:repeat(20,1fr)] aspect-square h-fit">
                 <For
-                    each=move || cells.clone().into_iter()
-                    key=|(id, _, _)| *id
-                    view=move |cx, (_, state, set_state)| {
+                    each=move || cells.clone().into_iter().enumerate()
+                    key=|(id, _)| *id
+                    view=move |cx, (_, (state, set_state))| {
                         view! {
                             cx,
                             <div
                                 class="p-4 w-fit h-fit grid place-items-center rounded-sm transition-colors"
                                 class=("bg-slate-500", move || state.get() == game_of_life::CellState::Dead)
-                                on:click=move |_| with_state(move |state| set_state(, !state.get()))
+                                on:click=move |_| set_state.lock().unwrap()()
                             />
                         }
                     }
