104e0333059bd0d4dd67ed5ee3f32548d01bfceb
Alan Randon <alan.randon@outlook.com>
2023-12-08 18:21:35 +0000

    Make steps move

--- crates/wgpu-experiments/src/main.rs
+++ crates/wgpu-experiments/src/main.rs
@@ -3,9 +3,10 @@
 use image::RgbaImage;
 use pipeline::Vertex;
 use renderer::texture::Texture;
-use renderer::{bind_group, buffer, Renderer, Window};
+use renderer::{bind_group, buffer, Encoder, Renderer, Window};
+use std::time::Duration;
 use winit::event::{ElementState, KeyEvent, WindowEvent};
-use winit::event_loop::EventLoop;
+use winit::event_loop::EventLoopBuilder;
 use winit::keyboard::{Key, NamedKey};
 use winit::window::WindowBuilder;
 
@@ -24,6 +25,7 @@ struct Globals {
     window_size: [f32; 2],
 }
 
+#[derive(Debug, Clone, Copy)]
 struct Step {
     x: f32,
     y: f32,
@@ -31,41 +33,150 @@ struct Step {
 
 impl Step {
     const WIDTH: f32 = 0.1;
+    const INDEX: [u16; 6] = [0, 1, 2, 0, 2, 3];
 
-    fn get_buffers(&self, texture: &Texture, device: &wgpu::Device) -> ([Vertex; 4], [u16; 6]) {
+    fn vertex(&self, texture: &Texture) -> [Vertex; 4] {
         let texture: &RgbaImage = texture.as_ref();
         let (width, height) = texture.dimensions();
         let half_width = Self::WIDTH / 2.;
         let half_height = half_width * height as f32 / width as f32;
 
-        (
-            [
-                Vertex {
-                    position: [self.x - half_width, self.y - half_height],
-                    tex_coords: [0., 1.],
+        let x = self.x;
+        let y = self.y;
+
+        [
+            Vertex {
+                position: [x - half_width, y - half_height],
+                tex_coords: [0., 1.],
+            },
+            Vertex {
+                position: [x + half_width, y - half_height],
+                tex_coords: [1., 1.],
+            },
+            Vertex {
+                position: [x + half_width, y + half_height],
+                tex_coords: [1., 0.],
+            },
+            Vertex {
+                position: [x - half_width, y + half_height],
+                tex_coords: [0., 0.],
+            },
+        ]
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Event {
+    Tick,
+    Resize(winit::dpi::PhysicalSize<u32>),
+}
+
+struct State {
+    step_texture: Texture,
+    step_index: buffer::Index,
+    steps: Vec<(Step, buffer::Vertex<Vertex>)>,
+    globals: bind_group::Single<Globals>,
+}
+
+impl State {
+    const SIZE: [f32; 2] = [1., 1.];
+
+    fn new(renderer: &mut Renderer) -> Self {
+        let step_texture = Texture::new(
+            &image::load_from_memory(include_bytes!("../assets/step.png")).unwrap(),
+            renderer,
+        );
+
+        let step_index = buffer::Index::create(&renderer.device, &Step::INDEX);
+
+        let globals = bind_group::Single::new(
+            renderer,
+            Globals {
+                size: Self::SIZE,
+                window_size: [100., 100.],
+            },
+        );
+
+        Self {
+            step_index,
+            steps: (0..10)
+                .map(|_| {
+                    let step = Step {
+                        x: rand::random(),
+                        y: 0.,
+                    };
+
+                    (
+                        step,
+                        buffer::Vertex::create(&renderer.device, &step.vertex(&step_texture)),
+                    )
+                })
+                .collect(),
+            globals,
+            step_texture,
+        }
+    }
+
+    fn handle_event(&mut self, event: Event, device: &wgpu::Device, queue: &wgpu::Queue) {
+        match event {
+            Event::Resize(size) => self.globals.update(
+                queue,
+                Globals {
+                    size: Self::SIZE,
+                    window_size: [size.width as f32, size.height as f32],
                 },
-                Vertex {
-                    position: [self.x + half_width, self.y - half_height],
-                    tex_coords: [1., 1.],
-                },
-                Vertex {
-                    position: [self.x + half_width, self.y + half_height],
-                    tex_coords: [1., 0.],
-                },
-                Vertex {
-                    position: [self.x - half_width, self.y + half_height],
-                    tex_coords: [0., 0.],
-                },
-            ],
-            [0, 1, 2, 0, 2, 3],
-        )
+            ),
+            Event::Tick => {
+                for step in &mut self.steps {
+                    step.0.y += 0.01;
+                    step.1.update(queue, step.0.vertex(&self.step_texture));
+                }
+            }
+        }
+    }
+
+    fn render(&self, encoder: &mut Encoder) {
+        let view = encoder.view();
+        let mut render_pass = encoder
+            .render_pass(&wgpu::RenderPassDescriptor {
+                label: None,
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.0,
+                            g: 0.0,
+                            b: 0.0,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            })
+            .step::<pipeline::Pipeline>();
+
+        render_pass
+            .render_pass
+            .set_bind_group(0, self.globals.as_ref(), &[]);
+
+        render_pass
+            .render_pass
+            .set_bind_group(1, self.step_texture.as_ref(), &[]);
+
+        for step in &self.steps {
+            render_pass.draw_indexed(&step.1, &self.step_index);
+        }
     }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
-    let event_loop = EventLoop::new().unwrap();
+    let event_loop = EventLoopBuilder::with_user_event().build().unwrap();
     let window = WindowBuilder::new()
         .with_title("WGPU fun")
         .build(&event_loop)?;
@@ -76,26 +187,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
     let (mut window, device, queue) = Window::new(&instance, &window);
 
-    let mut globals = bind_group::Single::new(
-        &mut window.renderer,
-        Globals {
-            window_size: [window.size.width as f32, window.size.height as f32],
-            size: [1., 1.],
-        },
-    );
+    let mut state = State::new(&mut window.renderer);
+    state.handle_event(Event::Resize(window.size), &device, &queue);
 
-    let step_texture = Texture::new(
-        &image::load_from_memory(include_bytes!("../assets/step.png")).unwrap(),
-        &mut window.renderer,
-    );
-    let mut step = Step { x: 0.5, y: 0.5 };
-    let (vertices, indices) = step.get_buffers(&step_texture, &device);
-    let (mut vertex, mut index) = (
-        buffer::Vertex::create(device.as_ref(), &vertices),
-        buffer::Index::create(device.as_ref(), &indices),
-    );
+    let proxy = event_loop.create_proxy();
+
+    std::thread::spawn(move || loop {
+        let _ = proxy.send_event(Event::Tick);
+        std::thread::sleep(Duration::from_millis(50));
+    });
 
     event_loop.run(move |event, elwt| match event {
+        winit::event::Event::UserEvent(event) => {
+            state.handle_event(event, &device, &queue);
+            window.window.request_redraw();
+        }
         winit::event::Event::WindowEvent {
             window_id: id,
             event,
@@ -107,60 +213,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ..
             } => match (logical_key, state) {
                 (Key::Named(NamedKey::Escape), ElementState::Pressed) => elwt.exit(),
-                (Key::Named(NamedKey::ArrowLeft), ElementState::Pressed) => step.x -= 0.01,
-                (Key::Named(NamedKey::ArrowRight), ElementState::Pressed) => step.x += 0.01,
                 _ => {}
             },
             WindowEvent::CloseRequested => elwt.exit(),
             WindowEvent::Resized(size) => {
-                globals.update(
-                    &queue,
-                    Globals {
-                        window_size: [size.width as f32, size.height as f32],
-                        size: [1., 1.],
-                    },
-                );
+                state.handle_event(Event::Resize(size), &device, &queue);
                 window.resize(size);
             }
             WindowEvent::RedrawRequested => {
-                let (vertices, indices) = step.get_buffers(&step_texture, &device);
-                vertex.update(queue.as_ref(), &vertices);
-                index.update(queue.as_ref(), &indices);
-
-                let result = window.renderer.with_encoder(|mut encoder| {
-                    let view = encoder.view();
-                    let mut render_pass = encoder
-                        .render_pass(&wgpu::RenderPassDescriptor {
-                            label: None,
-                            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                                view: &view,
-                                resolve_target: None,
-                                ops: wgpu::Operations {
-                                    load: wgpu::LoadOp::Clear(wgpu::Color {
-                                        r: 0.0,
-                                        g: 0.0,
-                                        b: 0.0,
-                                        a: 1.0,
-                                    }),
-                                    store: wgpu::StoreOp::Store,
-                                },
-                            })],
-                            depth_stencil_attachment: None,
-                            occlusion_query_set: None,
-                            timestamp_writes: None,
-                        })
-                        .step::<pipeline::Pipeline>();
-
-                    render_pass
-                        .render_pass
-                        .set_bind_group(0, globals.as_ref(), &[]);
-
-                    render_pass
-                        .render_pass
-                        .set_bind_group(1, step_texture.as_ref(), &[]);
-
-                    render_pass.draw_indexed(&vertex, &index);
-                });
+                let result = window
+                    .renderer
+                    .with_encoder(|mut encoder| state.render(&mut encoder));
 
                 if let Err(err) = result {
                     match err {
